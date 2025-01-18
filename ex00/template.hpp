0bf5eb13cc0c3b3d9a762151df20b9cdf1b5922e
Timur Schmidt <timur.schmidt.ts@gmail.com>
2025-01-18 13:46:59 +0100

    started with ex00, templates

---
+++ ex00/template.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   template.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: timschmi <timschmi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/18 13:35:11 by timschmi          #+#    #+#             */
+/*   Updated: 2025/01/18 13:43:38 by timschmi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+//Template functions
+
+template<typename T> 
+void swap(T &a, T &b)
+{
+	T temp = a;
+
+	a = b;
+	b = temp;
+}
+
+template<typename T>
+T min(T a, T b)
+{
+	if (a < b)
+		return (a);
+	else
+		return (b);
+}
+
+template<typename T>
+T max(T a, T b)
+{
+	if (a > b)
+		return (a);
+	else
+		return (b);
+}
